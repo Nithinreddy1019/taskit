595beba0bbf085c9dc28b20317a24ac52fb92043
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-10-22 16:10:32 +0530

    Auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -12,7 +12,7 @@ const AuthLayout = ({
         <main className="min-h-screen">
             <div className="max-w-2xl mx-auto p-4">
                 <AuthNav />
-                <div className="flex flex-col items-center justify-center pt-4 md:pt-10">
+                <div className="flex flex-col items-center justify-center pt-4 md:pt-6">
                     {children}
                 </div>
             </div>
