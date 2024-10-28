5fade2eda6b1111cf01cb77c285adbd5efd00985
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-10-28 23:55:19 +0530

    UX navigation

--- components/dashboard/navigation.tsx
+++ components/dashboard/navigation.tsx
@@ -1,13 +1,17 @@
+"use client"
+
 import { GoHome, GoHomeFill, GoCheckCircle, GoCheckCircleFill } from "react-icons/go";
 import { SettingsIcon, UsersIcon } from "lucide-react"
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { usePathname } from "next/navigation";
 
 
 const routes = [
     {
         label: "Home",
-        href: "/home",
+        href: "",
         icon: GoHome,
         activeIcon: GoHomeFill
     },
@@ -33,20 +37,28 @@ const routes = [
 
 
 export const Navigation = () => {
+
+    const workspaceId = useWorkspaceId();
+    const pathname = usePathname();
+
+
     return (
         <ul className="flex flex-col">
             {routes.map((item) => {
-                const isActive = false;
+
+                const fullHref = `/workspaces/${workspaceId}${item.href}`;
+
+                const isActive = pathname === fullHref;
                 const Icon = isActive ? item.activeIcon : item.icon;
 
                 return (
                     <Link
                         key={item.href}
-                        href={item.href}
+                        href={fullHref}
                     >
                         <div className={cn(
                             "flex items-center gap-2.5 p-2.5 rounded-md font-medium text-neutral-500 hover:text-primary transition",
-                            isActive && ("bg-white shadow-sm hover:opacity-100 text-primary")
+                            isActive && ("bg-white shadow-sm hover:opacity-100 text-primary dark:bg-black/40")
                         )}>
                             <Icon className="size-5 text-neutral-500 "/>
                             {item.label}
