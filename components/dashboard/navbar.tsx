e88851dbc02b12d98817abbd93c256c905698162
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-11-20 13:57:14 +0530

    Navbar bug fix

--- components/dashboard/navbar.tsx
+++ components/dashboard/navbar.tsx
@@ -1,13 +1,23 @@
+"use client"
+import { usePathname } from "next/navigation"
 import { MobileSidebar } from "./mobile-sidebar"
 import { UserButton } from "./user-button"
 
 
     export const Navbar = () => {
+        // WIP:: Update the name as per the path in url
+
+        const pathname = usePathname();
+        console.log(pathname)
+
+        const navHeading = pathname.includes("/tasks") ? "All Tasks" : pathname.includes("/projects") ? "Project" : "Home";
+        const navDesc = pathname.includes("/tasks") ? "Monitor all your tasks" : pathname.includes("/projects") ? "Monitor your project here" : "Monitor all your tasks and projects here.";
+
         return (
             <nav className="pt-4 px-6 flex items-center justify-between w-full">
                 <div className="hidden lg:flex flex-col">
-                    <h1 className="text-2xl font-semibold">Home</h1>
-                    <p className="text-muted-foreground">Monitor all your tasks and projects here.</p>
+                    <h1 className="text-2xl font-semibold">{navHeading}</h1>
+                    <p className="text-muted-foreground">{navDesc}</p>
                 </div>
 
                 <MobileSidebar />
